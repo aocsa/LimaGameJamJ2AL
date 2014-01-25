9918636c4997270d4b91d796680c2fc68ac46d86
Alexander Ocsa <aocsa.cs@gmail.com>
2014-01-25 13:53:10 -0500

    ...

--- LimaGameJam/Classes/HelloWorldScene.m
+++ LimaGameJam/Classes/HelloWorldScene.m
@@ -5,6 +5,7 @@
 //  Created by DSBMobile on 25/01/14.
 //  Copyright LimaGameJam 2014. All rights reserved.
 //
+// hello world!!!
 // -----------------------------------------------------------------------
 
 #import "HelloWorldScene.h"
